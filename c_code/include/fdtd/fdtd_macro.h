b3a0cbcd0679c10f8e9c1cc897b8329ae69058a8
Ben Cerjan <bcerjan@gmail.com>
2020-03-27 12:09:45 -0500

    Added License file and licensing headers to all files

--- c_code/include/fdtd/fdtd_macro.h
+++ c_code/include/fdtd/fdtd_macro.h
@@ -1,3 +1,22 @@
+/**
+    Copyright (c) 2020 Ben Cerjan
+
+    This file is part of simpleFDTD.
+
+    simpleFDTD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    simpleFDTD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with simpleFDTD.  If not, see <https://www.gnu.org/licenses/>.
+**/
+
 #ifndef FDTD_MACRO
 #define FDTD_MACRO
 
