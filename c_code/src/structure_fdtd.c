3c899bad19ad73afb5407fd9bc3ee880cf959fcd
Ben Cerjan <bcerjan@gmail.com>
2020-04-01 12:55:06 -0500

    Updated initial simulation window size

--- c_code/src/structure_fdtd.c
+++ c_code/src/structure_fdtd.c
@@ -37,7 +37,7 @@ int main() {
 
   printf( "Allocated Grid\n" );
 
-  InitializeFdtd(g, 0, 1, 2000.0, 1.0); // First int for metal, second for object shape
+  InitializeFdtd(g, 4, 1, 2000.0, 1.0); // First int for metal, second for object shape
   printf( "Initialized Grid\n" );
 
   maximumIteration = NUMBEROFITERATIONCONSTANT;
