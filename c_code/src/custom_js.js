90ccf3ca5c26fce200007dffae1cc837f2d28ade
Ben Cerjan <bcerjan@gmail.com>
2020-04-03 15:06:47 -0500

    Updated to unlock normalization button after a run as well as run button

--- c_code/src/custom_js.js
+++ c_code/src/custom_js.js
@@ -65,7 +65,6 @@ mergeInto(LibraryManager.library, {
 mergeInto(LibraryManager.library, {
   updateChartData: function() {
     outputChart.update();
-    console.log(outputChart);
   }
 });
 
@@ -73,6 +72,7 @@ mergeInto(LibraryManager.library, {
 mergeInto(LibraryManager.library, {
   enableButton: function() {
     runSim.disabled = false;
+    normData.disabled = false;
     runSim.innerHTML = "Run Simulation";
   }
 });
