4b8aa19149c5b34bca26f82621cc92d76202f5c4
Ben Cerjan <bcerjan@gmail.com>
2020-06-09 10:44:32 -0500

    Removed unnecessary variables and fixed error due to calling abcSize when it was no longer defined

--- c_code/src/grid_initialization.c
+++ c_code/src/grid_initialization.c
@@ -84,23 +84,12 @@ void  InitializeFdtd (struct Grid *g, int metalChoice, int objectChoice,
     double  mediaConductivity[MEDIACONSTANT] = {0.0, getMatConductivity(metalChoice)}; // sig,
     double  mediaPermeability[MEDIACONSTANT] = {1.0, getMatPermeability(metalChoice)};    // mur
     double  mediaResistivity[MEDIACONSTANT] = {0.0, getMatResistivity(metalChoice)};     // sim
-    double  drudePlasma[MEDIACONSTANT] = {0.0};
-    double  drudeDamping[MEDIACONSTANT] = {HUGE_VAL}; // initialize vacuum to have huge damping
-    complex double  mediaA[MEDIACONSTANT][MAX_POLES] = {0.0};
-    complex double  mediaC[MEDIACONSTANT][MAX_POLES] = {0.0};
+    complex double  mediaA[MEDIACONSTANT][MAX_POLES] = {{0.0}};
+    complex double  mediaC[MEDIACONSTANT][MAX_POLES] = {{0.0}};
     double  magneticPermeability0,electricalPermittivity0,frequency,wavelength,angularFrequency;
-    double  reflectionCoefficient0,gradingOrder,temporary,electricalImpedance0,temp1,temp2,temp3;
-    int  i,j,k,p, boundaryDataSize, media, boundaryIndex,xSizeMain,ySizeMain,numFreqs;
-    int  abcSize ;
-    double  temporaryi,temporaryj,distance2 ;
+    double  temporary,electricalImpedance0;
+    int  i,j,p,media,xSizeMain,ySizeMain,numFreqs;
     int  xCenter,yCenter;
-    double  x,x1,x2;
-    double  electricalConductivityMaximum, kappaMaximum, boundaryWidth, alphaPML;
-    double  gradientConductivity, gradientResistivity, boundaryFactor, denominator;
-    double  gradientCa1[ABCSIZECONSTANT];
-    double  gradientCb1[ABCSIZECONSTANT];
-    double  gradientDa1[ABCSIZECONSTANT];
-    double  gradientDb1[ABCSIZECONSTANT];
 
     /***********************************************************************/
     //     Fundamental constants
@@ -121,7 +110,8 @@ void  InitializeFdtd (struct Grid *g, int metalChoice, int objectChoice,
     dx = 10.0e-9; // 10 nm
     double dxnm = dx*1e9; // Grid step size in nm
 
-    courantS = 1.0/2.0;
+    //courantS = 1.0/2.0;
+    courantS = 4.0/2.0;
     dt = courantS * dx / speedOfLight;
 
     absConst = (speedOfLight*dt - dx) / (speedOfLight*dt + dx);
@@ -134,19 +124,13 @@ void  InitializeFdtd (struct Grid *g, int metalChoice, int objectChoice,
 
     /*xSizeMain = 300;                              // number of main grid cells in x-direction
     ySizeMain = 250;*/
-    xSizeMain = 200;
-    ySizeMain = 150;                               // number of main grid cells in y-direction
-    abcSize = ABCSIZECONSTANT;                    // thickness of PML region
-    xSize = xSizeMain + 2 * abcSize;              // number of total grid cells in x-direction
-    ySize = ySizeMain + 2 * abcSize;              // number of total grid cells in y-direction
+    xSizeMain = 300;
+    ySizeMain = 250;                               // number of main grid cells in y-direction
+    //abcSize = ABCSIZECONSTANT;                    // thickness of PML region
+    xSize = xSizeMain; //+ 2 * abcSize;              // number of total grid cells in x-direction
+    ySize = ySizeMain; //+ 2 * abcSize;              // number of total grid cells in y-direction
 
-    boundaryDataSize  = 2 * xSize * abcSize;                      // front edge + back edge
-    boundaryDataSize += 2 * (abcSize * (ySize - 2 * abcSize));    // left + right edges
-
-    xSource = 50 + abcSize;                          //location of z-directed hard source
-    //ySource = 50 + abcSize;                          //location of z-directed hard source
-    //xSource = 20 + abcSize;                       //location of z-directed hard source
-    ySource = ySize / 2;                          //location of z-directed hard source
+    xSource = 20;                          //location of z-directed hard source
 
     envIndex = environmentIndex;                  // Background refractive index
 
@@ -165,6 +149,10 @@ void  InitializeFdtd (struct Grid *g, int metalChoice, int objectChoice,
     // Number of poles in our dielectric function
     if (metalChoice > -1 && objectChoice > -1) {
       number_poles = materialData[metalChoice].num_poles;
+      for (p = 0; p < number_poles; p++) {
+        mediaA[1][p] = materialData[metalChoice].params[p].ap;
+        mediaC[1][p] = materialData[metalChoice].params[p].cp;
+      }
     } else {
       number_poles = 0;
     }
@@ -219,8 +207,8 @@ void  InitializeFdtd (struct Grid *g, int metalChoice, int objectChoice,
     reHzTranDFT = AllocateMemory(numFreqs, ySize, 0.0);
     imHzTranDFT = AllocateMemory(numFreqs, ySize, 0.0);
 
-    reflXPos = abcSize + 5;
-    tranXPos = xSize - abcSize - 5;
+    reflXPos = 5;
+    tranXPos = xSize - 5;
 
     double waveMin, waveMax; // Min / Max wavelength in PPW
     waveMin = (400.0e-9) / dx; // 400 nm
@@ -319,7 +307,7 @@ void  InitializeFdtd (struct Grid *g, int metalChoice, int objectChoice,
     object_locs = AllocateMemory(xSize, ySize, 0.0); // This really shouldn't be an array of doubles -- we're wasting memory here
 
     // Initialize structure functions:
-    xCenter = xSize - abcSize - 30; // In grid units
+    xCenter = xSize - 30; // In grid units
     yCenter = ySize / 2; // ""
     structInit(xCenter, yCenter);
 printf("Strucutre Init...\n" );
@@ -382,81 +370,14 @@ printf("Strucutre Init...\n" );
     freeComplexDoublePtr(qC2, media);
 
 printf("Structure Added...\n" );
-
+printf("Structure Choice: %i\n", objectChoice);
+printf("objectXSize: %f\n", objectXSize);
+printf("object_locs[xCent][yCent]: %f\n", object_locs[xCenter][yCenter]);
 
     /***********************************************************************/
-    //     Fill the PML with lossy terms
+    //     Tridiagonal Solver Coefficients
     /***********************************************************************/
-
-    // The most important part of the PML fdtd simulation is getting the
-    // PML Coefficients correct. Which requires getting the correct PML gradient and
-    // positioning the coefficients correctly on the x-y grid.
-
-    // ALERT: It is possible to make a mistake here, and yet the simulation may appear to
-    // be working properly. However a detailed analysis of reflections off the PML
-    // will show they may be (much) larger than those for a correctly designed PML.
-
-    gradingOrder = 4.0;                             // for PML, (m) was 2;  optimal values: 2 <= m <= 6,  Nikolova part4 p.29
-
-    double gradKx[ABCSIZECONSTANT] = {0.0};
-    double gradKy[ABCSIZECONSTANT] = {0.0};
-    double kMax = 4.0;
-    PMLkx = AllocateMemory(xSize, ySize, 1.0); // Multipled anywhere we have d^2 / dx^2 derivatives
-    PMLky = AllocateMemory(xSize, ySize, 1.0); // " but for y-derivatives
-    //PMLkz = AllocateMemory(xSize, ySize, 1.0); // This is always 1.0, but it's nice to have for symmetry
-
-
-    boundaryWidth = (double  )abcSize * dx;    // width of PML region (in mm)
-    double prefactor = (kMax-1.0)/(dx * pow(boundaryWidth,gradingOrder)) * (gradingOrder + 1.0);
-
-    // build the gradient
-    //  caution: if the gradient is built improperly, the PML will not function correctly
-    for (i = 0, x = 0.0; i < abcSize; i++, x++) {
-        x1 = (x + 0.5) * dx;       // upper bounds for point i
-        x2 = (x - 0.5) * dx;       // lower bounds for point i
-        if (i == 0) {
-            temporary = 1.0 + prefactor * (pow(x1,(gradingOrder+1))  );   //   polynomial grading  (special case: on the edge, 1/2 = pml, 1/2 = vacuum)
-        } /* if */
-        else {
-            temporary = 1.0 + prefactor * (pow(x1,(gradingOrder+1)) - pow(x2,(gradingOrder+1)) );   //   polynomial grading
-        } /* else */
-        gradKy[i] = temporary;
-
-        x1 = (x + 1.0) * dx;       // upper bounds for point i
-        x2 = (x + 0.0) * dx;       // lower bounds for point i
-        gradKx[i] = 1.0 + prefactor * (pow(x1,(gradingOrder+1)) - pow(x2,(gradingOrder+1)) );   //   polynomial grading
-
-    } /* iForLoop */
-
-    printf("Order: %f\n", gradingOrder);
-    printf("gradKx[0]: %.17g\n", gradKx[0]);
-    printf("gradKx[7]: %.17g\n", gradKx[7]);
-
-    // Now, loop through PML regions and set PML-k terms where appropriate:
-    // Left/Right PML Region:
-    /*for (i = 0; i < abcSize; i++) {
-      for (j = 0; j < ySize; j++) {
-        PMLkx[abcSize - 1 - i][j]     = 1.0/gradKx[i];
-        PMLkx[xSize - abcSize + i][j] = 1.0/gradKx[i];
-      }
-    }
-
-    // Top/Bottom
-    for (j = 0; j < abcSize; j++) {
-      for (i = 0; i < xSize; i++) {
-        PMLky[i][abcSize - 1 - j]     = 1.0/gradKy[j];
-        PMLky[i][ySize - abcSize + j] = 1.0/gradKy[j];
-      }
-    }*/
-    /*
-    printf("PMLkx[7][133]: %.17g\n", PMLkx[7][133]);
-    printf("ABCConst*PMLkx[7][133]: %.17g\n", ABConst[7][133]*PMLkx[7][133]);
-    printf("PMLkx[0][0]: %.17g\n", PMLkx[0][0]);
-    printf("ABCConst*PMLkx[0][0]: %.17g\n", ABConst[0][0]*PMLkx[0][0]);
-    */
-
-    // Finally, set values for our tridiagonal solver(s)
-    // This needs to be done last as they take the values from the PML where necessary
+    // This needs to be done last as they take the values from above
 
     // For dx^2 terms (used in hzTriDiagonalSolve):
     ahz = AllocateMemory(xSize, ySize, 0.0);
@@ -470,15 +391,22 @@ printf("Structure Added...\n" );
 
     for (i = 0; i < xSize; i++) {
       for (j = 0; j < ySize; j++) {
-        ahz[i][j] = -1.0 * PMLkx[i][j]*ABConst[i][j];
-        bhz[i][j] = iConst1[i][j] + 2.0*PMLkx[i][j]*ABConst[i][j];
+        ahz[i][j] = -1.0 * ABConst[i][j];
+        //bhz[i][j] = iConst1[i][j] + 2.0*ABConst[i][j];
+        bhz[i][j] = 1.0 + 2.0*ABConst[i][j]; // no e(w) term for Hz update
         chz[i][j] = ahz[i][j];
 
-        aex[i][j] = -1.0 * PMLky[i][j]*ABConst[i][j];
-        bex[i][j] = iConst1[i][j] + 2.0*PMLky[i][j]*ABConst[i][j];
+        aex[i][j] = -1.0 * ABConst[i][j];
+        bex[i][j] = iConst1[i][j] + 2.0*ABConst[i][j];
         cex[i][j] = aex[i][j];
       }
     }
+
+    /***********************************************************************/
+    //     Mur ABC Setup
+    /***********************************************************************/
+
+
 
     // Add Mur ABC conditions from "Mur Absorbing Boundary Condition for 2-D
     // Leapgfrog ADI-FDTD Method", Gan and Tan, IEEE Asia-Pacific Conf. 2012
@@ -505,12 +433,12 @@ printf("Structure Added...\n" );
       chz[xSize-1][j] = 0.0;
     }
 
-    printf("ahz: %.17g\n", ahz[4][0]);
+/*    printf("ahz: %.17g\n", ahz[4][0]);
     printf("bhz: %.17g\n", bhz[4][0]);
 
     printf("aex: %.17g\n", aex[4][0]);
     printf("bex: %.17g\n", bex[4][0]);
-
+*/
 
     // all done with Initialization!
 
