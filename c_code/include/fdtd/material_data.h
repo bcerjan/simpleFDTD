46c20aba29a66e188d9e887eb28331962d7ce645
Ben Cerjan <bcerjan@gmail.com>
2020-06-18 11:45:23 -0500

    Updated all materials to the new fitting function

--- c_code/include/fdtd/material_data.h
+++ c_code/include/fdtd/material_data.h
@@ -44,7 +44,7 @@
 #define PLANCK_H 4.135667e-15 // eV/sec
 #define EPS0 8.854187e-12
 #define SPEEDC 2.99792458e8 // m/sec
-#define CONVERSION 6.28319/PLANCK_H // Conversion for constants that have units of energy (2 PI/h) to get to rad/sec
+#define CONVERSION 6.28319/PLANCK_H // Conversion for constants that have units of energy (2 Pi/h) to get to rad/sec
 
 struct cpParams {
   complex double ap;
@@ -65,17 +65,16 @@ struct Material {
 static const struct Material materialData[6] = {
   { //0, Al, Vial, 2011
     .num_poles = 2,
-    .epsInf = 0.329666,
-    //.epsInf = 1.329666,
+    .epsInf = 1.0,
     .permeability = 1.0,
-    .conductivity = 254.536 * EPS0 * CONVERSION,
+    .conductivity = 413.505 * EPS0 * CONVERSION,
     .params[0] = {
-      .ap = (-0.0783386 + I*0.0699538)*CONVERSION,
-      .cp = (-128.853 - I*732.243)*CONVERSION,
+      .ap = (-0.272493 + I*1.44484)*CONVERSION,
+      .cp = (3.80577 - I*5.88315)*CONVERSION,
     },
     .params[1] = {
-      .ap = (-0.255539 - I*1.46655)*CONVERSION,
-      .cp = (2.81621 + I*5.36337)*CONVERSION,
+      .ap = (-0.116623 + I*0.133088)*CONVERSION,
+      .cp = (-209.786 - I*295.234)*CONVERSION,
     },
   },
   { //1, Au, Johnson and Christy
@@ -94,54 +93,54 @@ static const struct Material materialData[6] = {
   },
   { //2, Ag, Wu, 2014
     .num_poles = 2,
-    .epsInf = 2.17741,
+    .epsInf = 3.35141,
     .permeability = 1.0,
-    .conductivity = 1.0e12,
+    .conductivity = 105.955 * EPS0 * CONVERSION,
     .params[0] = {
-      .ap = (-0.388929 - I*2.22844)*CONVERSION,
-      .cp = (4.26389 + I*8.17685)*CONVERSION,
+      .ap = (-0.063629 + I*0.154755)*CONVERSION,
+      .cp = (-55.7092 - I*260.36)*CONVERSION,
     },
     .params[1] = {
-      .ap = (-0.388929 - I*2.22844)*CONVERSION,
-      .cp = (4.26389 + I*8.17685)*CONVERSION,
+      .ap = (5.26744 - I*3.11234)*CONVERSION,
+      .cp = (-17.5676 - I*34.0369)*CONVERSION,
     },
   },
   { //3, Cu, J&C
     .num_poles = 2,
-    .epsInf = 1.82316,
+    .epsInf = 1.98801,
     .permeability = 1.0,
-    .conductivity = 1.0e12,
+    .conductivity = 27.8466 * EPS0 * CONVERSION,
     .params[0] = {
-      .ap = (-0.388929 - I*2.22844)*CONVERSION,
-      .cp = (4.26389 + I*8.17685)*CONVERSION,
+      .ap = (-0.607993 + I*2.0456)*CONVERSION,
+      .cp = (3.84516 + I*1.37118)*CONVERSION,
     },
     .params[1] = {
-      .ap = (-0.388929 - I*2.22844)*CONVERSION,
-      .cp = (4.26389 + I*8.17685)*CONVERSION,
+      .ap = (-0.0173655 - I*0.134845)*CONVERSION,
+      .cp = (-9.84239 + I*261.159)*CONVERSION,
     },
   },
-  { //4, SiO2, Lemarchand, 2013
+  { //4, SiO2
     .num_poles = 0,
-    .epsInf = 2.22439,
+    .epsInf = 2.2201, // constant n = 1.49
     .permeability = 1.0,
-    .conductivity = 1.0e-10, // needs to be non-0 for stability
+    .conductivity = 0.0,
   },
   { //5, Si, Green, 2008
     .num_poles = 3,
-    .epsInf = 0.793124,
+    .epsInf = 1.30676,
     .permeability = 1.0,
-    .conductivity = 1.0e12,
+    .conductivity = 0.0471157 * EPS0 * CONVERSION,
     .params[0] = {
-      .ap = (-0.388929 - I*2.22844)*CONVERSION,
-      .cp = (4.26389 + I*8.17685)*CONVERSION,
+      .ap = (-0.116424 + I*3.3587)*CONVERSION,
+      .cp = (1.96042 - I*2.20821)*CONVERSION,
     },
     .params[1] = {
-      .ap = (-0.388929 - I*2.22844)*CONVERSION,
-      .cp = (4.26389 + I*8.17685)*CONVERSION,
+      .ap = (-0.381842 + I*4.26879)*CONVERSION,
+      .cp = (-0.578006 - I*13.8223)*CONVERSION,
     },
     .params[2] = {
-      .ap = (-0.388929 - I*2.22844)*CONVERSION,
-      .cp = (4.26389 + I*8.17685)*CONVERSION,
+      .ap = (-0.37732 - I*3.6481)*CONVERSION,
+      .cp = (1.38221 + I*4.59924)*CONVERSION,
     },
   },
 };
